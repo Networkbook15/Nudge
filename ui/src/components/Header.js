7e853af88079f58966a7fc60e179f913fe0fb973
Will Kelly <willkelly0@gmail.com>
2018-03-16 10:30:49 -0400

    navigation to static pages

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -1,11 +1,47 @@
 import React from "react";
+import {Nav, Navbar, NavDropdown, MenuItem, NavItem} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const dummyPublicKey = '0xc02B48f6b5847c6d5aC4a2EEf3283D7436295788';
 
 const Header = (props) => {
-    return (
-        <div className="container">
-            <h1>Header</h1>
-        </div>
-    );
+  return (
+    <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">Nudge</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>  
+    
+    <Navbar.Text>
+      Signed in as: {dummyPublicKey}
+    </Navbar.Text>
+      
+
+    <Nav pullRight>
+      <LinkContainer to="/judging">
+        <NavItem eventKey={1}>Judging Panel</NavItem>         
+      </LinkContainer>
+      <NavDropdown eventKey={2} title="Commitments" id="basic-nav-dropdown">
+        <LinkContainer to="/newcommitment">
+          <MenuItem eventKey={2.1}> New Commitment</MenuItem>
+        </LinkContainer>
+        <MenuItem divider />
+        <LinkContainer to="/mycommitments">
+          <MenuItem eventKey={2.2}>My Commitments</MenuItem>
+        </LinkContainer>
+      </NavDropdown>
+        <LinkContainer to="/community">
+          <NavItem eventKey={3}>Community</NavItem>
+        </LinkContainer>
+      </Nav>
+
+
+    </Navbar.Collapse>
+    </Navbar>    
+  );
 }
 
 export default Header;
